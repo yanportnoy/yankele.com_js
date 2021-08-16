a6fa6039edf69cb7515050b364ade5a3c596e35b
yanportnoy <yankl@yankele.com>
2021-08-15 23:06:24 -0400

    202108152306

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
-const navBarElements = document.getElementById("navBarItems");
-navBarElements.addEventListener("click", onItemClick);
-console.log(navBarElements);
-Array.from(navBarElements).forEach(function (item) {
-  console.log(item.id);
-});
-
+const navBarElements = document.querySelectorAll("a");
+const navBarElement = document.getElementById("navBarItems");
+navBarElement.addEventListener("click", onItemClick);
+// console.log(navBarElements);
+// for (let i = 0; i < navBarElements.length; i++) {
+//   console.log(navBarElements[i]);
+// }
 function onItemClick(e) {
-  console.log(e.target.id);
+  // console.log(e.target);
+  for (let i = 0; i < navBarElements.length; i++) {
+    navBarElements[i].classList.remove("active");
+  }
   //navBarElements.forEach();
   e.target.setAttribute("class", "active");
+}
+
+function load_home() {
+  document.getElementById("content").innerHTML =
+    '<object type="text/html" data="home.html" ></object>';
 }
