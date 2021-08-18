b4e0a56d5777d75a1898ce2e7fcb30945ec13f29
yanportnoy <yankl@yankele.com>
2021-08-18 13:56:29 -0400

    202108181356

--- main.js
+++ main.js
@@ -12,12 +12,21 @@ function onItemClick(e) {
   }
   //navBarElements.forEach();
   e.target.setAttribute("class", "active");
+  getHTML(e.target.id);
+
   var fileName =
     `<object class="h-100 d-inline-block w-100" style="width: 100%" type="text/html" data=html/` +
     e.target.id +
     `.html>"`;
   console.log(fileName);
   document.getElementById("content").innerHTML = fileName;
+}
+
+function getHTML(id) {
+  page = "html/" + id + ".html";
+  fetch(page)
+    .then((data) => data.text())
+    .then((html) => (document.getElementById("content").innerHTML = html));
 }
 
 function load_home() {
