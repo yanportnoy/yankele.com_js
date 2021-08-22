bb836e53e24711495a6797b32b67608756eca077
yanportnoy <yankl@yankele.com>
2021-08-21 22:32:54 -0400

    202108212232

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 const navBarElements = document.querySelectorAll("a");
 const navBarElement = document.getElementById("navBarItems");
+
 navBarElement.addEventListener("click", onItemClick);
+const links = [
+  "https://lh3.googleusercontent.com/31JLCohGB26StrE6oSSHdTBPZKMlxBijWpS8QTNl9RoBVuSmViJVEdwIZZkUSHavbQUuXR3VXwB8fYLIkpp-7BYPwUr5DQuGUc6XuzvET3FAJ7olVDbvoewy8cJYRBwp-IXjM71pccI=w1920-h1080",
+  "https://lh3.googleusercontent.com/31JLCohGB26StrE6oSSHdTBPZKMlxBijWpS8QTNl9RoBVuSmViJVEdwIZZkUSHavbQUuXR3VXwB8fYLIkpp-7BYPwUr5DQuGUc6XuzvET3FAJ7olVDbvoewy8cJYRBwp-IXjM71pccI=w1920-h1080",
+  "https://lh3.googleusercontent.com/dGRnMAz0E54vwi9gj3ae2G6qnkJ4QSlDIt_hpHWKrW9WUXxIcH5Ng2bFuXnJTALETjM-7l3F9CFO49HXRfSBeqRo2_VZn4SnOQ8ye2gyzI03ckXVnWRDaOrH-tNpA2bKaT7Kn7m2NSA=w1920-h1080",
+  "https://lh3.googleusercontent.com/XZrQ-WYZHlAOxWmc6R7FtAxFFbDALgFxWOzjYTw8bX0X25N5SGJ615722PvgtKP3VAU2bKaqy4u0svsTlrpNxJYD7AwHQfo63aOic8S6PUXFdQDD2FIQRNjLxGB-1NjdWo9WZi8ugVQ=w1920-h1080",
+  "https://lh3.googleusercontent.com/Ecdjcq5HKOzRegdJt77zxdn1eDkfKuQBqe-Wn9b0cBYG2ChnBkbSIVwKb-ZZ3-1DQ6noJ7GGpRICvo0UiLtsVhnjnXZZmu0pYkMjAAKmtugD_oRE-K1h9wNtlhJjbWMkeQ50dtHjm6E=w1920-h1080",
+  "https://lh3.googleusercontent.com/DzDYaqHsPZtDUVePlfdFUVWJ9YCuE6D10muFANlKv7uoiyDr-fikjlGwVGTOGjRaRnXXQqGP8Q2jSWML1ffPCbF1jl4ZBAfR7GauzJNN0sq4DzAUBCbF6hsTIcIeHfadjBxBCvfS-SE=w1920-h1080",
+  "https://lh3.googleusercontent.com/yfjy0WXblhG6CFUUsOEi3GfHu2JVIbMXzYa9yTyj3974aWsTJWYuteAbjZK30jG2lcmMDSKlLh_MMEkaUAoHjWe07rGD88CtilU65ySrIvVEmFVgUciJaU2DBp0NgakudNJNFkHi3ZQ=w1920-h1080",
+];
 
 function onItemClick(e) {
   // console.log(e.target);
@@ -10,7 +20,11 @@ function onItemClick(e) {
   //navBarElements.forEach();
   e.target.setAttribute("class", "active");
   getHTML(e.target.id);
-
+  if (e.target.id === "photo") {
+    const circle = document.getElementsByClassName("bx");
+    console.log(circle);
+  }
+  console.log(e.target.id);
   // var fileName =
   //   `<object class="h-100 d-inline-block w-100" style="width: 100%" type="text/html" data=html/` +
   //   e.target.id +
